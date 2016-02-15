1f682fcbd9ed3026beda17099f6867897852649e
paulgoblin <mpr75@cornell.edu>
2016-02-15 11:49:56 -0800

    API to heroku url

--- public/js/module.js
+++ public/js/module.js
@@ -4,6 +4,7 @@ var app = angular.module('testApp', ['ui.router'])
 
 
 .constant('CONST', {
-  API_URL: 'http://localhost:3000',
+  // API_URL: 'http://localhost:3000',
+  API_URL: 'https://sleepy-brushlands-39846.herokuapp.com',
   THUMBNAIL_API: 'https://damp-reaches-8819.herokuapp.com/image/thumbnail/'
 })
